fa6c899a2a933bf4dd970d4a19eb78459feeba69
unknown <augusto.carneirojr@gmail.com>
2020-06-26 14:50:44 +0000

    refactoring

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import AddExpensePage from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
-import NotFoundPage from '../components/NotFoundPage';
-import LoginPage from '../components/LoginPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
+import React from "react";
+import { Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+import BreakDashboardPage from "../components/BreakDashboardPage";
+import AddBreakPage from "../components/AddBreakPage";
+import EditBreakPage from "../components/EditBreakPage";
+import NotFoundPage from "../components/NotFoundPage";
+import LoginPage from "../components/LoginPage";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createHistory();
 
@@ -16,9 +16,9 @@ const AppRouter = () => (
     <div>
       <Switch>
         <PublicRoute path="/" component={LoginPage} exact={true} />
-        <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
-        <PrivateRoute path="/create" component={AddExpensePage} />
-        <PrivateRoute path="/edit/:id" component={EditExpensePage} />
+        <PrivateRoute path="/dashboard" component={BreakDashboardPage} />
+        <PrivateRoute path="/create" component={AddBreakPage} />
+        <PrivateRoute path="/edit/:id" component={EditBreakPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,17 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import expensesReducer from '../reducers/expenses';
-import filtersReducer from '../reducers/filters';
-import authReducer from '../reducers/auth';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import breaksReducer from "../reducers/breaks";
+import filtersReducer from "../reducers/filters";
+import authReducer from "../reducers/auth";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({
-      expenses: expensesReducer,
+      breaks: breaksReducer,
       filters: filtersReducer,
-      auth: authReducer
+      auth: authReducer,
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
